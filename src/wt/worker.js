ee41605d0723ee62eaaa5e37ad3f310b3ba53e3a
yuliya-d98 <feelgloom@gmail.com>
2026-03-09 02:35:58 +0300

    feat: worker task

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -6,4 +6,7 @@ import { parentPort } from 'worker_threads';
 
 parentPort.on('message', (data) => {
   // Write your code here
+
+	const sorted = [...data].sort((a, b) => a - b);
+	parentPort.postMessage(sorted);
 });
